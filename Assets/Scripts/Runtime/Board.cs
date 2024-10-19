9a862e76b709459c3c35ee7da5d3a69dbd377b07
hailuu <hoanghai.luu.71@gmail.com>
2024-10-19 17:35:12 +0700

    chore: generate board

---
+++ Assets/Scripts/Runtime/Board.cs
@@ -0,0 +1,49 @@
+﻿namespace Runtime
+{
+    using System;
+    using UnityEngine;
+
+    public class Board : MonoBehaviour
+    {
+        public Transform         TilesParent;
+        public         GameObject TilePrefab;
+        private static int        Rows    = 8;
+        private static int        Columns = 8;
+
+        private Tile[,] runtimeTiles = new Tile[Board.Rows, Board.Columns];
+
+        private void Awake() { this.SpawnTiles(); }
+
+        private void SpawnTiles()
+        {
+            for (var row = 0; row < Rows; row++)
+            {
+                for (var column = 0; column < Columns; column++)
+                {
+                    var tile = Instantiate(this.TilePrefab, this.TilesParent).GetComponent<Tile>();
+                    tile.Row    = row;
+                    tile.Column = column;
+                    this.runtimeTiles[row, column]   = tile.GetComponent<Tile>();
+                    this.runtimeTiles[row, column].OnTileClicked += this.OnTileClicked;
+                }
+            }
+        }
+
+        private void OnTileClicked(int row, int column)
+        {
+            this.ShowDistanceNeighbours(row, column);
+        }
+
+        private void ShowDistanceNeighbours(int row, int column)
+        {
+            for (var r = row - 1; r <= row + 1; r++)
+            {
+                for (var c = column - 1; c <= column + 1; c++)
+                {
+                    if (r < 0 || r >= Rows || c < 0 || c >= Columns || (r == row && c == column)) continue;
+                    this.runtimeTiles[r, c].TxtIndex.text = $"{Math.Sqrt(Math.Pow(Math.Abs(r - row), 2) + Math.Pow(Math.Abs(c - column), 2)) * 10:F0}";
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Tile.cs
@@ -0,0 +1,27 @@
+﻿namespace Runtime
+{
+    using TMPro;
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using UnityEngine.UI;
+
+    public delegate void TileClicked(int row, int column);
+
+    public class Tile : MonoBehaviour, IPointerDownHandler
+    {
+        public int      Row;
+        public int      Column;
+        public TMP_Text TxtIndex;
+        public Image    ImgBg;
+
+        public event TileClicked OnTileClicked;
+
+        private void Awake() { this.TxtIndex.text = string.Empty; }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            this.ImgBg.color = Color.cyan;
+            this.OnTileClicked?.Invoke(this.Row, this.Column);
+        }
+    }
+}
